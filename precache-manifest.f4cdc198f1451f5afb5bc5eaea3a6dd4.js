983eaa4353fc17a5c8785c9a979425ae0bb8c06f
Derrick Lee <gorick2491@hotmail.com>
2020-11-30 23:12:44 -0800

    Updates

---
+++ precache-manifest.f4cdc198f1451f5afb5bc5eaea3a6dd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d1a7363464505e2b3d8bd561c9ebe3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67a3eceb792f10a093eb",
+    "url": "/static/css/main.4f4d2a49.chunk.css"
+  },
+  {
+    "revision": "e7e9b75e55557b9016d6",
+    "url": "/static/js/2.01055415.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.01055415.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67a3eceb792f10a093eb",
+    "url": "/static/js/main.89216bcb.chunk.js"
+  },
+  {
+    "revision": "5d9298c6aa860f2f85c5",
+    "url": "/static/js/runtime-main.ea3247fb.js"
+  },
+  {
+    "revision": "9474c698eddcf7f4aa6496960183fe20",
+    "url": "/static/media/profile.9474c698.jpg"
+  }
+]);
